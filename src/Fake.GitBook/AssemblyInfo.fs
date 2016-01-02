d0eca0bb9bd6ae0c8db61799195952ad46c441f9
pocketberserker <pocketberserker@gmail.com>
2016-01-02 20:19:45 +0900

    Bump version to 0.0.2

--- src/Fake.GitBook/AssemblyInfo.fs
+++ src/Fake.GitBook/AssemblyInfo.fs
@@ -6,10 +6,10 @@ open System.Runtime.InteropServices
 [<assembly: AssemblyProductAttribute("FAKE.GitBook")>]
 [<assembly: GuidAttribute("de681942-cfcc-41d6-ac28-b1c9a5859679")>]
 [<assembly: AssemblyDescriptionAttribute("FAKE extension for GitBook")>]
-[<assembly: AssemblyVersionAttribute("0.0.1")>]
-[<assembly: AssemblyFileVersionAttribute("0.0.1")>]
-[<assembly: AssemblyInformationalVersionAttribute("0.0.1")>]
+[<assembly: AssemblyVersionAttribute("0.0.2")>]
+[<assembly: AssemblyFileVersionAttribute("0.0.2")>]
+[<assembly: AssemblyInformationalVersionAttribute("0.0.2")>]
 do ()
 
 module internal AssemblyVersionInformation =
-    let [<Literal>] Version = "0.0.1"
+    let [<Literal>] Version = "0.0.2"
